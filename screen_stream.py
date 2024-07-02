c8aac6c6068cffd8c5c76ee7ca7620c746911a93
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-07-02 04:30:32 +0200

    refactor: Add screen streaming functionality

---
+++ screen_stream.py
@@ -0,0 +1,88 @@
+import cv2
+import numpy as np
+from flask import Flask, Response
+from flask_cors import CORS
+import mss
+import mss.tools
+
+app = Flask(__name__)
+CORS(app)
+
+
+def generate_frames():
+    with mss.mss() as sct:
+        # Obtiene las dimensiones del monitor
+        monitor = sct.monitors[1]
+
+        while True:
+            # Captura la pantalla
+            sct_img = sct.grab(monitor)
+
+            # Convierte la imagen a un array numpy
+            img = np.array(sct_img)
+
+            # Convierte de BGRA a BGR
+            img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+
+            # Codifica la imagen a formato JPEG
+            ret, buffer = cv2.imencode(".jpg", img, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
+            frame = buffer.tobytes()
+
+            # Genera el frame en el formato adecuado
+            yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
+
+
+@app.route("/video_feed")
+def video_feed():
+    return Response(
+        generate_frames(), mimetype="multipart/x-mixed-replace; boundary=frame"
+    )
+
+
+@app.route("/")
+def index():
+    return """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Screen Stream</title>
+        <style>
+            body, html {
+                margin: 0;
+                padding: 0;
+                width: 100vw;
+                height: 100vh;
+                background-color: black;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                overflow: hidden; /* Para eliminar el scroll */
+            }
+            .video-container {
+                width: 100vw; /* Ajusta este valor según el tamaño deseado */
+                height: 100vh; /* Mantener la proporción 4:3 */
+                position: relative;
+            }
+            .video-container img {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                object-fit: contain; /* Cambiado a 'contain' para ajustar la imagen sin recortarla */
+            }
+        </style>
+    </head>
+    <body>
+        <div class="video-container">
+            <img src="/video_feed" alt="Video Stream" />
+        </div>
+    </body>
+    </html>
+    """
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, threaded=True)
